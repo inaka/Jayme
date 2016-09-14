432df285d81ca03bc01d52fa3269aa514ff54d55
pablolvillar <pablo@inakanetworks.com>
2016-09-14 12:20:50 -0300

    Removed not needed custom logger.

--- Example/AppDelegate.swift
+++ Example/AppDelegate.swift
@@ -27,9 +27,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        Jayme.Logger.sharedLogger.loggingFunction = { (items, separator, terminator) in
-            print(items, separator: separator, terminator: terminator)
-        }
         return true
     }
 
